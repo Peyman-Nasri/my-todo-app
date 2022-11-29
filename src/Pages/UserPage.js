56234d2fca21d70190b278c7c1e8c4d773cdb57e
dreamylife1988 <peyman.nasri@yahoo.com>
2022-11-29 13:07:33 +0100

    show user information in new page

--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -1,10 +1,11 @@
-import { Card } from "@mui/material";
+import styled from "@emotion/styled";
+import { Card, LinearProgress } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const UserPage = () => {
-  const [FetchingData, setFetchingData] = useState(false);
+  const [FetchingData, setFetchingData] = useState(true);
   const [user, setUser] = useState(false);
 
   const { userId } = useParams();
@@ -21,20 +22,44 @@ const UserPage = () => {
   };
 
   useEffect(() => {
-    getUser()
-  }, [])
+    getUser();
+  }, []);
 
   return (
     <>
       <h1>User Page</h1>
-      <Card style={{ margin: "1em auto", padding: "1em" }}>
-        <div>
-          <b>ID: </b>
-          <span>{userId}</span>
-        </div>
-      </Card>
+      {FetchingData ? (
+        <LinearProgress />
+      ) : (
+        // <Card style={{ margin: "1em auto", padding: "1em", lineHeight: '1.5em' }}>
+        <StyledCard>
+          <div>
+            <b>Username: </b>
+            <span>{user.username}</span>
+          </div>
+          <div>
+            <b>Email: </b>
+            <span>{user.email}</span>
+          </div>
+          <div>
+            <b>Address: </b>
+            <span>{`${user.address?.street} - ${user.address?.suite} - ${user.address?.city}`}</span>
+          </div>
+          <div>
+            <b>Zipcode: </b>
+            <span>{user.address.zipcode}</span>
+          </div>
+        </StyledCard>
+      )}
     </>
   );
 };
 
 export default UserPage;
+
+
+const StyledCard = styled(Card)`
+  margin: 1.5 auto;
+  line-height: 1.5em;
+  padding: 1em;
+`
